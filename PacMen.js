801ebf41f0322799846f177e6db39f3330324b4c
Martin Grajeda Jr <martingrajedajr@gmail.com>
2022-02-16 17:54:06 -0600

    Update PacMen.js

--- PacMen.js
+++ PacMen.js
@@ -2,7 +2,7 @@ var pos = 0;
     const pacArray = [
         'PacMan1.png', 'PacMan2.png',
         'PacMan3.png', 'PacMan4.png',
-        'PacMan5.png
+        'PacMan5.png'
     ];
     var direction = 0;
     const pacMen = []; // This array holds all the pacmen
